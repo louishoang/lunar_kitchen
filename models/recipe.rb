ae46f6168385c6e81695e468a8dc315aee074137
louis hoang <bawsing@gmail.com>
2014-09-05 14:11:09 -0400

    recipes list done

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,35 @@
-class Recipe
+def db_connection
+    begin
+    connection = PG.connect(dbname: 'recipes')
+    yield(connection)
+    ensure
+      connection.close
+  end
 end
+
+def get_data(query)
+  db_connection do |conn|
+    conn.exec(query)
+  end
+end
+
+class Recipe
+  attr_accessor :id, :name
+
+  def initialize(recipe)
+    @id = recipe["id"]
+    @name = recipe["name"]
+    @instructions = recipe["instructions"]
+    @description = recipe["description"]
+  end
+
+  def self.all
+    query = 'SELECT * FROM recipes'
+    results = get_data(query).to_a
+    results.map do |result|
+      recipe = Recipe.new(result)
+    end
+  end
+end
+
+
